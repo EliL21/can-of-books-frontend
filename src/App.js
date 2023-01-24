ca10d2ab4cdd87103af023f3807572b7481d48b1
eli bailey <baileyeli19@gmail.com>
2023-01-24 11:15:51 +0000

    update

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+import Bestbooks from './Bestbooks';
+import Header from './Header';
+import About from './About';
+import Footer from './Footer';
+import Button from 'react-bootstrap/Button'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      showModal: false
+    }
+  }
+
+  getBooks = async () => {
+    try {
+      // TODO: use axios to call out to my server get all the booksfrom the DB
+      let url = `${process.env.REACT_APP_SERVER}/books`
+
+      let bookData = await axios.get(url);
+
+      this.setState({
+        books: bookData.data
+      });
+
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+  // **** Handle Delete Request *********
+  deleteBooks = async(id) => {
+    let url = `${process.env.REACT_APP_SERVER}/books/${id}`
+
+    await axios.delete(url);
+    let filterBooks = this.state.books.filter(book => book._id !== id)
+    this.setState ({
+      books: filterBooks
+    })
+  }
+  // REACT LIFECYCLE METHOD 
+
+  componentDidMount() {
+    this.getBooks();
+  }
+// HandleOpenModal that change setState  to true
+// Handler to close the modal
+// Handler to grab form data and make my post request
+
+
+  render() {
+    console.log('App State >>> ', this.state);
+    return (
+      <>
+      <Router>
+          <Header />
+          <Routes>
+            <Route
+              exact path='/'
+              element={
+              <>
+              <Button>Add a Book</Button>
+              <Bookformmodal />
+              <Bestbooks books={this.state.books} deleteBooks={this.deleteBooks} />
+              </>
+              }
+
+            >
+            </Route>
+            <Route
+              exact path='/about'
+              element={<About />}
+            >
+            </Route>
+            {/* PLACEHOLDER: add a route with a path of ‘/about’ that renders the `About` component */}
+          </Routes>
+          <Footer />
+        </Router>
+      
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/Bestbooks.js
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button'
+
+
+
+class Bestbooks extends React.Component {
+
+
+
+
+
+
+  render() {
+
+    return (
+      <>
+
+        <title>Bestbooks</title>
+        {/* <Carousel slide={}>
+        {this.state.books.map}
+      </Carousel> */}
+        {this.props.books.length ?
+          <Carousel>
+            {this.props.books.map(book => (
+
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="./bookimg.jpg"
+                  alt="Carousel background"
+                />
+                <Carousel.Caption>
+                  <h3>{book.title}</h3>
+                  <p>{book.description}</p>
+                  <Button onClick= {() =>{this.props.deleteBooks(book._id)}}>Delete</Button>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+          :
+          <h3>No books found</h3>
+  }
+
+      </>
+    )
+  }
+}
+
+export default Bestbooks;
